8210f7b435e9f4f9d15d4bcaeab135e84c228cac
Guillaume Jouvet <guillaume.jouvet@gmail.com>
2025-03-17 23:46:28 +0100

    upt run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -1,7 +1,12 @@
 
-for A in aletsch-1880-2100 aletsch-basic aletsch-invert Bueler2005C paleo-alps quick-demo quick-demo-mysmb rhone-invert synthetic
+for A in aletsch-1880-2100 aletsch-basic aletsch-invert Bueler2005C paleo-alps synthetic
 do
    cd $A
    igm_run +experiment=params
    cd ..
 done
+
+cd quick-demo
+   igm_run +experiment=params_1
+   igm_run +experiment=params_2
+cd ..
