c9b81ff2f3ee2516c1ae489bbd7033afb53cb53c
Guillaume Jouvet <guillaume.jouvet@gmail.com>
2025-03-24 09:55:55 +0100

    upt param invert

--- run_all.sh
+++ run_all.sh
@@ -2,11 +2,11 @@
 for A in aletsch-1880-2100 aletsch-basic aletsch-invert Bueler2005C paleo-alps synthetic
 do
    cd $A
-   igm_run +experiment=params
+   igm_run +experiment=params core.print_comp=True
    cd ..
 done
 
 cd quick-demo
-   igm_run +experiment=params_1
-   igm_run +experiment=params_2
+   igm_run +experiment=params_1 core.print_comp=True
+   igm_run +experiment=params_2 core.print_comp=True
 cd ..
