b6870e20f2e5d480d529308e1063cc537de12d8f
Guillaume Jouvet <guillaume.jouvet@gmail.com>
2025-07-18 23:06:23 +0200

    addjust seeding routine

--- aletsch-1880-2100/user/code/processes/particles/particles.py
+++ aletsch-1880-2100/user/code/processes/particles/particles.py
@@ -32,15 +32,24 @@ def seeding_particles(cfg, state):
     """
  
     I = (state.thk>1)&(state.seeding>0.5)&state.gridseed  # here you may redefine how you want to seed particles
-    state.nparticle_x = state.X[I] - state.x[0]  # x position of the particle
-    state.nparticle_y = state.Y[I] - state.y[0]  # y position of the particle
-    state.nparticle_z = state.usurf[I]  # z position of the particle
-    state.nparticle_r = tf.ones_like(state.X[I])  # relative position in the ice column
-    state.nparticle_w = tf.ones_like(state.X[I])  # weight of the particle
-    state.nparticle_t = (tf.ones_like(state.X[I])*state.t)  # "date of birth" of the particle (useful to compute its age)
-    state.nparticle_englt = tf.zeros_like(state.X[I])  # time spent by the particle burried in the glacier
-    state.nparticle_thk = state.thk[I]  # ice thickness at position of the particle
-    state.nparticle_topg = state.topg[I]  # z position of the bedrock under the particle
+
+    state.nparticle["x"] = state.X[I] - state.x[0]  # x position of the particle
+    state.nparticle["y"] = state.Y[I] - state.y[0]  # y position of the particle
+    state.nparticle["z"] = state.usurf[I]           # z position of the particle
+    state.nparticle["thk"] = state.thk[I]           # ice thickness at position of the particle
+    state.nparticle["topg"] = state.topg[I]         # z position of the bedrock under the particle
+
+    state.nparticle["t"] = tf.ones_like(state.nparticle["x"]) * state.t
+    state.nparticle["r"] = (state.nparticle["z"] - state.nparticle["topg"]) / state.nparticle["thk"]
+    state.nparticle["r"] = tf.where(state.nparticle["thk"] == 0, tf.ones_like(state.nparticle["r"]), state.nparticle["r"])
+
+    if "weights" in cfg.processes.particles.fields:
+        state.nparticle["w"] = tf.ones_like(state.nparticle["x"])
+    if "englt" in cfg.processes.particles.fields:
+        state.nparticle["englt"] = tf.zeros_like(state.nparticle["x"])
+    if "velmag" in cfg.processes.particles.fields:
+        state.nparticle["velmag"] = tf.zeros_like(state.nparticle["x"])
+
 
 # override the official seeding_particles function
-igm.processes.particles.update_tf.seeding_particles = seeding_particles
+igm.processes.particles.update_particles.seeding_particles_user = seeding_particles
